bcdadfd0cd2e319b6eb622c7ddfa975b522db55b
amirren <amirren@caci.co.uk>
2021-04-27 14:57:58 +0100

    del

--- f.bat
+++
@@ -1,8 +0,0 @@
-echo "# synergy4-docker" >> README.md
-git init
-git add README.md
-git add Dockerfile
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/amirren/synergy4-docker.git
-git push -u origin main
